9190f79ca80c53e3a2e0d87b6929db2e4db89cb6
francis-ux <kingmicheal744@gmail.com>
2025-05-03 18:07:03 +0100

    directives and attributes bindings

--- tags_input/src/components/TagsInput.vue
+++ tags_input/src/components/TagsInput.vue
@@ -1,3 +1,16 @@
 <template>
-    <input type="text">
+    <div v-for="(tag, index) in tags">{{ index + " : " + tag }}</div>
+    <input type="text" :value="newTag" />
 </template>
+
+
+<script>
+export default {
+    data() {
+        return {
+            tags: ["Vue", "React", "Angular"],
+            newTag: "Preact"
+        }
+    }
+}
+</script>
