969b3d251c386c41a1d1331a4edd0e9fd1ae2e20
francis-ux <kingmicheal744@gmail.com>
2025-07-09 13:28:21 +0100

    watchers'

--- tags_input/src/components/TagsInput.vue
+++ tags_input/src/components/TagsInput.vue
@@ -20,6 +20,15 @@ export default {
         }
     },
 
+    watch: {
+        newTag(newVal) {
+            if (newVal.indexOf(",") > -1) {
+                this.newTag = this.newTag.slice(0, -1)
+                this.addNewTag();
+            }
+        }
+    },
+
     computed: {
         isTagExists() {
             return this.tags.includes(this.newTag)
